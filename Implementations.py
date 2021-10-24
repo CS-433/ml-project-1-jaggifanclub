81fbfd7d6e40915f93fad4928ddc298a92489284
loconsta <loris.constantin@epfl.ch>
2021-10-24 12:09:47 +0200

    ridge and least_squares done

--- Implementations.py
+++ Implementations.py
@@ -360,10 +360,10 @@ def params_optimization(y, x, k_fold, model, degrees, lambdas = None, params = N
                 degree_accs_te.append(acc_te)
                 degree_losses_tr.append(loss_tr)
                 degree_losses_te.append(loss_te)
-            accs_tr.append(np.mean(lambda_accs_tr))
-            accs_te.append(np.mean(lambda_accs_te))
-            losses_tr.append(np.mean(lambda_losses_tr))
-            losses_te.append(np.mean(lambda_losses_te))
+            accs_tr.append(np.mean(degree_accs_tr))
+            accs_te.append(np.mean(degree_accs_te))
+            losses_tr.append(np.mean(degree_losses_tr))
+            losses_te.append(np.mean(degree_losses_te))
     else:
     # Get a mean accuracy value (by cross-validation) for each degree-lambda combination
         for degree in degrees:
@@ -401,12 +401,13 @@ def params_optimization(y, x, k_fold, model, degrees, lambdas = None, params = N
             losses_te.append(degree_losses_te)
               
     return np.array(losses_tr), np.array(losses_te), np.array(accs_tr), np.array(accs_te)
-              
+ 
+    
 """"""""""""""""""""""""
 "  PLOTTING FUNCTIONS  "
 """"""""""""""""""""""""
 
-def plot_param_vs_loss(params, err_tr, err_te, param = 'degree', err_type = 'MSE', model_name = 'model', saveimg = False, img_name = '-1'):
+def plot_param_vs_err(params, err_tr, err_te, model_name = 'model', err_type = 'MSE', param = 'degree', save_img = False, img_name = '-1'):
     """
     Visualization of the curves of mse/accuracy given parameter (degree, lambda or other).
      :param params: list of the parameters used for each version of the model
@@ -415,12 +416,12 @@ def plot_param_vs_loss(params, err_tr, err_te, param = 'degree', err_type = 'MSE
      :param param: label of the parameter used
      :param err_type: type of error (mse or accuracy)
      :param model_name: name of the model used
-     :param saveimg: boolean indicating if the image generated must be saved
+     :param save_img: boolean indicating if the image generated must be saved
      :param img_name: if the image must be saved, name demanded (in order to not erase previously saved images)
     """
     if err_type == 'MSE' or err_type == 'mse':
         best_idx = np.argmin(err_te)
-    elif err_type == 'accuracy' or err_type == 'ACCURACY':
+    elif err_type == 'accuracy' or err_type == 'Accuracy' or err_type == 'ACCURACY':
         best_idx = np.argmax(err_te)
     
     if param == 'lambda':
@@ -435,14 +436,14 @@ def plot_param_vs_loss(params, err_tr, err_te, param = 'degree', err_type = 'MSE
     plt.title(err_type + ' of ' + model_name + ' given different values for parameter: ' + param)
     plt.legend()
     plt.grid(True)
-    if saveimg:
+    if save_img:
         if img_name == '-1':
             print('Argument not found: img_name. Image not saved.')
         else:
-            fig.savefig('figures/' + img_name)
+            plt.savefig('figures/' + img_name)
     plt.show()
 
-def plot_param_vs_loss_and_acc(params, loss_tr, loss_te, acc_tr, acc_te, param = 'degree', model_name = 'model', save_img = False, img_name = '-1'):
+def plot_param_vs_loss_and_acc(params, loss_tr, loss_te, acc_tr, acc_te, model_name = 'model', param = 'degree', save_img = False, img_name = '-1'):
     """
     Visualization of the curves of loss AND accuracy given parameter (degree, learning rate, lambda).
      :param params: list of the parameters used for each version of the model
@@ -452,7 +453,7 @@ def plot_param_vs_loss_and_acc(params, loss_tr, loss_te, acc_tr, acc_te, param =
      :param acc_te: corresponding test accuracy
      :param param: label of the parameter used
      :param model_name: name of the model used
-     :param saveimg: boolean indicating if the image generated must be saved
+     :param save_img: boolean indicating if the image generated must be saved
      :param img_name: if the image must be saved, name demanded (in order to not erase previously saved images)
     """
     
@@ -481,20 +482,20 @@ def plot_param_vs_loss_and_acc(params, loss_tr, loss_te, acc_tr, acc_te, param =
     axs[0].grid(True)
     axs[1].grid(True)
     fig.legend()
-    if saveimg:
+    if save_img:
         if img_name == '-1':
             print('Argument not found: img_name. Image not saved.')
         else:
             fig.savefig('figures/' + img_name)
     plt.show()
     
-def plot_boxplots(errors, model_names, err_type = 'MSE', saveimg = False, img_name = '-1'):
+def plot_boxplots(errors, model_names, err_type = 'MSE', save_img = False, img_name = '-1'):
     """
     Visualisation of the performance of models across folds.
      :param errors: array of losses/accruacies, such that each ROW contains the losses/accuracies of a same model on different folds (cross-validation)
      :param model_names: names of the models corresponding to each row
      :param err_type: type of error (loss or accuracy)
-     :param saveimg: boolean indicating if the image generated must be saved
+     :param save_img: boolean indicating if the image generated must be saved
      :param img_name: if the image must be saved, name demanded (in order to not erase previously saved images)
     """
     errors = errors.T
@@ -502,20 +503,20 @@ def plot_boxplots(errors, model_names, err_type = 'MSE', saveimg = False, img_na
     plt.legend([bp['medians'][0], bp['means'][0]], ['median', 'mean'])
     plt.title('Boxplot of the ' + err_type + ' models (' + str(np.array(errors).shape[1]) + ' folds)')
     plt.ylabel(err_type)
-    if saveimg:
+    if save_img:
         if img_name == '-1':
             print('Argument not found: img_name. Image not saved.')
         else:
-            fig.savefig('figures/' + img_name)
+            plt.savefig('figures/' + img_name)
     plt.show()
     
-def plot_twice_boxplots(losses, accuracies, model_names, saveimg = False, img_name = '-1'):
+def plot_twice_boxplots(losses, accuracies, model_names, save_img = False, img_name = '-1'):
     """
     Visualisation of the performance of models across folds.
      :param losses: array of losses. Each ROW contains the loss of a same model on different folds (cross-validation)
      :param accuracies: array of accuraciess. Each ROW contains the accuracy of a same model on different folds (cross-validation)
      :param model_names: names of the models corresponding to each row
-     :param saveimg: boolean indicating if the image generated must be saved
+     :param save_img: boolean indicating if the image generated must be saved
      :param img_name: if the image must be saved, name demanded (in order to not erase previously saved images)
     """
     losses = losses.T
@@ -527,14 +528,14 @@ def plot_twice_boxplots(losses, accuracies, model_names, saveimg = False, img_na
     bp = axs[1].boxplot(accuracies, labels = model_names, showmeans = True)
     axs[1].set_ylabel('Accuracy')
     fig.legend([bp['medians'][0], bp['means'][0]], ['median', 'mean'])
-    if saveimg:
+    if save_img:
         if img_name == '-1':
             print('Argument not found: img_name. Image not saved.')
         else:
             fig.savefig('figures/' + img_name)
     plt.show()
     
-def plot_heatmap(err_tr, err_te, degrees, lambdas, model_name, measure_type = 'Accuracy', saveimg = False, img_name = '-1'):
+def plot_heatmap(err_tr, err_te, degrees, lambdas, model_name, measure_type = 'Accuracy', save_img = False, img_name = '-1'):
     """
     Visualisation of accuracy/loss computed over all lambda-degrees combinations using a heatmap
     :param err_tr: matrix of losses/accuracies computed on training set
@@ -543,7 +544,7 @@ def plot_heatmap(err_tr, err_te, degrees, lambdas, model_name, measure_type = 'A
     :param lambdas: vector of all lambdas used to regularize training
     :param model_name: model type used to train on data and predict labels
     :param measure_type: Measure used to assess performance (MSE/NLL/Accuracy)
-    :param saveimg: boolean indicating if the image generated must be saved
+    :param save_img: boolean indicating if the image generated must be saved
     :param img_name: if the image must be saved, name demanded (in order to not erase previously saved images)
     """
     fig, axs = plt.subplots(1, 2, figsize = [15,8])
@@ -570,7 +571,7 @@ def plot_heatmap(err_tr, err_te, degrees, lambdas, model_name, measure_type = 'A
 
     axs[0].set_title("Train " + measure_type)
     axs[1].set_title("Test " + measure_type)
-    if saveimg:
+    if save_img:
         if img_name == '-1':
             print('Argument not found: img_name. Image not saved.')
         else:
